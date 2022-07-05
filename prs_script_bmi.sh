0e2801bc8897de223a58dacea2a1e66d657c755a
aprilhartley <77832842+aprilhartley@users.noreply.github.com>
2022-07-05 16:26:21 +0100

    Create prs_script_bmi.sh

---
+++ prs_script_bmi.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=jackknife-prs
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=28
+#SBATCH --time=2-00:00:00
+#SBATCH -e prs_error
+#SBATCH --partition=mrcieu,mrcieu2
+
+module load apps/plink/2.00
+module load apps/bgen/1.1.4
+
+bgen_pattern=/mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chrCHROM.bgen
+
+bgen_index_pattern=/mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chrCHROM.bgen.bgi
+
+snp_list=bmi_snps.txt
+temp_geno_prefix=temp_genos
+for chrom in {1..22}; do
+  chrom_padd=$(printf "%0*d\n" 2 $chrom)
+  inbgen=${bgen_pattern/CHROM/$chrom_padd}
+  inbgenidx=${bgen_index_pattern/CHROM/$chrom_padd}
+  bgenix -g $inbgen -i $inbgenidx -incl-rsids $snp_list > $temp_geno_prefix.$chrom_padd.bgen
+done
+cmd=""
+for chrom in {01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22}; do
+  cmd="${cmd} ${temp_geno_prefix}.${chrom}.bgen"
+done
+cat-bgen -g ${cmd} -og bmi_dosage.bgen
+
+
+plink2 \
+  --bgen bmi_dosage.bgen  \
+  --score locke_bmi_weights.txt cols=+scoresums \
+  --out bmi_score_ukbb
